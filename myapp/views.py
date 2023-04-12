785c3ef465c6269d126c247b0eb4a17062f93f93
benegative <khannikkey@gmail.com>
2023-04-12 17:27:34 +0530

    Added urls and First view with Dummy Data

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from myapp.models import Movie
 
-# Create your views here.
+
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies' : list(movies.values())
+    }
+    return JsonResponse(data)
