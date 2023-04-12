9ca20cf66e833277f6ba97a4805e3f13c00af97e
benegative <khannikkey@gmail.com>
2023-04-13 02:41:10 +0530

    Upgraded code api_view to APIView

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from myapp.api import views
-
+from myapp.api.views import MovieListAPI, MovieDetailAPI
 
 urlpatterns = [
-    path('', views.movie_list ),
-    path('<int:pk>',views.movie_detail),
+    path('', MovieListAPI.as_view()),
+    path('<int:pk>', MovieDetailAPI.as_view()),
 ]
+
+# urlpatterns = [
+#     path('', views.movie_list ),
+#     path('<int:pk>',views.movie_detail),
+# ]
