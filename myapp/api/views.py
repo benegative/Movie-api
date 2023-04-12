e472c16d1494d34f6c70d75cc4bb34d6991c2c5b
benegative <khannikkey@gmail.com>
2023-04-13 02:16:59 +0530

    POST PUT DELETE api Added

--- myapp/api/views.py
+++ myapp/api/views.py
@@ -1,17 +1,47 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from myapp.models import Movie
 from myapp.api.serializers import MovieSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    movies = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movies)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        try:
+            movies = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'ÉRROR':'Movie Not Found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movies)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
